3251d01bb8fa286380c43f3cb9842fce35b02275
mario-sanmartin <sanmartin.mario@outlook.com>
2020-10-19 01:36:02 -0300

    Comunicacion entre componentes

---
+++ proyectos/02-proyecto/src/components/AccionSaldo.vue
@@ -0,0 +1,22 @@
+<template>
+  <button> {{texto}}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        texto: String
+       
+    }
+
+}
+</script>
+
+<style scoped>
+button {
+  border-radius: 8px;
+  padding: 5px 10px;
+  color: #666;
+}
+
+</style>
---
+++ proyectos/02-proyecto/src/components/Cuenta.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <h2>Tipo de Cuenta : {{cuenta}}</h2>
+  <h2>Saldo : {{saldo}}</h2>
+  <h2>Estado : {{estado ? 'Activo':'Desactivada'}}</h2>
+  <div v-for="(servicio,index) in servicios" 
+  :key="index">
+  {{index+1}} - {{servicio}}
+
+  </div>
+  <accionSaldo texto="Aumentar Saldo"  @click="aumentar" />
+  <accionSaldo  texto="Disminuir Saldo" @click="disminuir"/>
+</div>
+</template>
+
+<script>
+import accionSaldo from './AccionSaldo';
+export default {
+    components:{
+        accionSaldo
+    },
+    data(){
+        return{
+            saldo: 1000,
+            cuenta: 'Visa',
+            estado:false,
+            servicios:['giro','abono','transferencia']
+        }
+    },
+    methods:{
+        aumentar(){
+            this.saldo =  this.saldo + 100;
+        },
+        disminuir(){
+            this.saldo =  this.saldo - 100;
+        }
+        
+    }
+
+}
+</script>
+
+<style scoped>
+h2{
+    color: #888;
+}
+
+
+</style>
