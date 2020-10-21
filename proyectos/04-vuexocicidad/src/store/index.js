87e4148db0927c51dc83b2fb6ea314023efe735b
mario-sanmartin <sanmartin.mario@outlook.com>
2020-10-21 01:08:13 -0300

    Condicional para el contador de vuex

--- proyectos/04-vuexocicidad/src/store/index.js
+++ proyectos/04-vuexocicidad/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    contador: 1000
+    contador: 50
   },
   mutations: {
   },
--- proyectos/04-vuexocicidad/src/views/Home.vue
+++ proyectos/04-vuexocicidad/src/views/Home.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Mi contador {{ $store.state.contador }}</h1>
+    <h1
+    :style="colorContador"
+    >
+      {{titulo}}:  {{contador}}
+    </h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
-  components: {
+  data(){
+    return{
+      titulo:'Mi contador desde Vuex'
+    }
+  },
+  computed:{
+    ...mapState(['contador']),
+    colorContador(){
+      return [this.contador > 100 ? {'color': 'green'}: {'color':'red'}]
+
+    }
   }
 }
 </script>
