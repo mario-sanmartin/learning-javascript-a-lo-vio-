6d17821319985f14a40c1898745c6b62aa69076f
mario-sanmartin <sanmartin.mario@outlook.com>
2020-10-27 01:26:16 -0300

    Usando las claves de acceso de nuestro apikey

---
+++ proyectos/08-crudfire/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firebase-firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlXC-ySBG5a6P_3mv6os0Odmh3_oz_6FQ",
+    authDomain: "depto269-30332.firebaseapp.com",
+    databaseURL: "https://depto269-30332.firebaseio.com",
+    projectId: "depto269-30332",
+    storageBucket: "depto269-30332.appspot.com",
+    messagingSenderId: "142026831897",
+    appId: "1:142026831897:web:9f1cace5abcaa9f75b5288"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ proyectos/08-crudfire/src/views/Inicio.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Inicio</h1>
+</template>
+
+<script>
+export default {
+name: 'Inicio'
+}
+</script>
+
+<style>
+
+</style>
